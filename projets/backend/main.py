5ea574ea678605935ee385542d99cbd63a8dd6b5
Gladrat <3368584+Gladrat@users.noreply.github.com>
2024-11-25 12:00:01 +0100

    update

---
+++ projets/backend/main.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/products', methods=['GET'])
+def get_products():
+    products = ["Product 01", "Product 02", "Product 03", "TV full-hd", "Coucou-depuis-BDD"]
+    return jsonify(products)
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- projets/e-commerce_spa/app_fetch.js
+++ projets/e-commerce_spa/app_fetch.js
@@ -1,17 +1,27 @@
 const root = document.querySelector("root");
 let cart = [];
 
-let products = ["Product 01", "Product 02", "Product 03", "Product 04", "TV full-HD"];
+// Coder un observer réactif sur la liste des produits
+
+let products = [];
+await fetch("http://localhost:5000/products")
+  .then((response) => response.json())
+  .then((data) => {
+    products = data;
+  })
+  .catch((error) =>
+    console.error("Erreur lors de la récupération des produits:", error)
+  );
 
 products.forEach((product, i) => {
   const productID = product.toLowerCase().replace(" ", "_");
-  console.log(productID);
 
   const productHTML = document.createElement("div");
   productHTML.id = productID;
   productHTML.className = "page";
   productHTML.style.display = i == 0 ? "block" : "none";
 
+  productHTML.innerHTML = `<h1>${product}<h1>`;
   const button = document.createElement("button");
   button.textContent = "Add to cart";
   button.addEventListener("click", () => addToCart(productID));
