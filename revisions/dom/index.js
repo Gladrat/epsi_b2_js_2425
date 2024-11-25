0cce5ee1ceb20fe9354c448d510a6e20eed89d52
Gladrat <3368584+Gladrat@users.noreply.github.com>
2024-11-25 10:32:38 +0100

    update

--- revisions/dom/index.js
+++ revisions/dom/index.js
@@ -6,24 +6,30 @@ const clear = document.querySelector("#clear");
 const tasks = ["", "Coder en C++", "Coder en Python", "Faire les courses"];
 console.log(tasks);
 
-function addTaskToDom(task) {
+function taskToDom(task) {
   if (task && typeof task === "string") {
     const li = document.createElement("li");
     li.textContent = task;
 
     const remove = document.createElement("button");
-    remove.textContent = "X";
+    remove.textContent = "REMOVE";
 
     remove.addEventListener("click", (event) => {
       li.remove();
     });
 
     li.append(remove);
-
     ul.append(li);
   }
 }
 
 tasks.forEach((task) => {
-  addTaskToDom(task);
+  taskToDom(task);
 });
+
+// Ajouter des tâches
+  // Soit en cliquant sur ADD soit avec la touche entrée dans l'input text
+  // Vider l'input et (ux) remettre le focus dans l'input text
+
+// Clear
+  // Supprime toutes les tâches
