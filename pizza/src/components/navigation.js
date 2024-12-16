031341e24d7aef26e0c943bb73527f904123b82f
Gladrat <3368584+Gladrat@users.noreply.github.com>
2024-12-16 11:00:34 +0100

    update

--- pizza/src/components/navigation.js
+++ pizza/src/components/navigation.js
@@ -3,6 +3,6 @@ import { createElement } from "../framework/core.js";
 export const Navigation = createElement(
   "nav",
   null,
-  createElement("a", { href: "#accueil", id: "lien-accueil" }, "Accueil"),
-  createElement("a", { href: "#panier" }, "Panier")
+  createElement("a", { href: "#accueil", class: "beautiful-link" }, "Accueil"),
+  createElement("a", { href: "#panier", class: "beautiful-link" }, "Panier")
 );
