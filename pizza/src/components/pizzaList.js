fc9553380b5e8eabac3692f8053989c91413ed32
Gladrat <3368584+Gladrat@users.noreply.github.com>
2024-12-17 08:20:11 +0100

    update

--- pizza/src/components/pizzaList.js
+++ pizza/src/components/pizzaList.js
@@ -1,9 +1,68 @@
 import { createElement } from "../framework/core.js";
 
+// Ajouter un bouton "Commander" sur chaque pizza
+// Ajouter un événement -> console.log("pizza") lors du click
+
 export const PizzaList = createElement(
   "ul",
-  createElement("li", null, "Pizza spéciale"),
-  createElement("li", null, "Pizza reine"),
-  createElement("li", null, "Pizza 4 fromages"),
-  createElement("li", null, "Pizza chorizo"),
+  createElement(
+    "li",
+    null,
+    "Pizza spéciale",
+    " ",
+    createElement(
+      "button",
+      {
+        onclick: () => {
+          console.log("Pizza spéciale");
+        },
+      },
+      "Commander"
+    )
+  ),
+  createElement(
+    "li",
+    null,
+    "Pizza reine",
+    " ",
+    createElement(
+      "button",
+      {
+        onclick: () => {
+          console.log("Pizza reine");
+        },
+      },
+      "+"
+    )
+  ),
+  createElement(
+    "li",
+    null,
+    "Pizza 4 fromages",
+    " ",
+    createElement(
+      "button",
+      {
+        onclick: () => {
+          console.log("Pizza reine");
+        },
+      },
+      "+"
+    )
+  ),
+  createElement(
+    "li",
+    null,
+    "Pizza chorizo",
+    " ",
+    createElement(
+      "button",
+      {
+        onclick: () => {
+          console.log("pizza");
+        },
+      },
+      "+"
+    )
+  )
 );
